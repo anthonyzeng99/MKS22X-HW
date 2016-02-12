fcc68aae2d42825eb275e8fbacf5dd37360e8838
Anthony Zeng <anthonyzeng99@gmail.com>
2016-02-12 13:16:46 -0500

    worked on KnightBoard

--- 03KnightsTour/KnightBoard.java
+++ 03KnightsTour/KnightBoard.java
@@ -1,22 +1,79 @@
 public class KnightBoard {
 
+    public static final boolean DEBUG = false;
     public int[][] board;
+    public int boardSize;
     public int border = 2;
 
     public KnightBoard(int n) {
-	board = new int[n + border][n + border];
+	boardSize = n;
+	board = new int[n + (2 * border)][n + (2 * border)];
 	for ( int row = 0; row < board.length; row++ ) {
-	    for ( int col = 0; col < board[rowIndex].length; col++ ) {
-		if ( (row >= border && row <= board.length - border) && (col >= border && col <= board[row].length) ) {
+	    for ( int col = 0; col < board[row].length; col++ ) {
+		if ( isBorder(row,col) ) {
 		    board[row][col] = -1;
+		} else {
+		    board[row][col] = 0;
 		}
 	    }
 	}
     }
 
 
-    public isBorder(int row, int col) {
-
+    private boolean isBorder(int row, int col) {
+	if ( row < border || row >= ( border + boardSize ) ) {
+	    return true;
+	}  else if ( col < border || col >= ( border + boardSize)  ) {
+	    return true;
+	} 
+	return false;
     }
+
+
+    public boolean solve() {
+	return false;
+    }
+
+
+
+    public void printSolution() {
+
+        int printStart;
+	int printStop;
+
+	if (DEBUG) {
+	    printStart = 0;
+	    printStop = board.length;
+	} else {
+	    printStart = border;
+	    printStop = ( boardSize + border);
+	}
+
+	for ( int row = printStart; row < printStop; row++ ) {
+	    System.out.println();
+	    System.out.println();
+	    for ( int col = printStart; col < printStop; col++ ) {
+		int boardValue = board[row][col];
+		if (boardValue < 0 || boardValue >= 10) {
+		    System.out.print("  " + Integer.toString(boardValue));
+		} else {
+		System.out.print("   " + Integer.toString(boardValue));
+		}
+	    }
+	}
+
+	System.out.println();
+	System.out.println();
+    }
+    
+    
+    public static void main(String[] args) {
+	
+	KnightBoard k = new KnightBoard(8);
+
+	k.printSolution();
+
+    } 
+
 
 }
