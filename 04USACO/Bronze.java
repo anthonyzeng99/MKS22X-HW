be2f788072aa219d2931dc67f82916da10db5d21
Anthony Zeng <anthonyzeng99@gmail.com>
2016-02-23 23:51:01 -0500

    added Bronze.java

---
+++ 04USACO/Bronze.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class Bronze {
+
+    private ArrayList<String> input= new ArrayList<String>;
+    private int maxHeightRow; //Row coordinate of max elevation of 3x3 grid
+    private int maxHeightCol; //Col coordinate of max elevation of 3x3 grid
+    private int[][] field;
+    private int[][] instructions;
+    private int finalLakeElevation;
+    
+    public Bronze(String inputFile) {
+	Scanner in = new Scanner(new File(inputFile));
+	
+    }
+
+    private void readInputFile() {
+
+    }
+
+    private int maxHeightOfGrid(int row, int col) {
+
+    }
+
+
+}
