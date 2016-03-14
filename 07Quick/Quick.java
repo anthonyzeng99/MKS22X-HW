8de0fb3d615a0c61859238d5f00f3542a7fb4cdc
Anthony Zeng <anthonyzeng99@gmail.com>
2016-03-14 07:46:19 -0400

    fixed some problems with the new quick sort and partition

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -32,16 +32,15 @@ public class Quick {
 
         return start;
     }
-    
+     
     private static int[] partition(int[] data, int left, int right) {
 	printArray(data);
 	int index = (int)(Math.random() * (right - (left + 1)));
 	int pivot = data[index];
-	System.out.println(data[index]);
         int dataIndex = left;
 	int[] tempArray = new int[right - left];
 	int start = 0;
-        int end = tempArray.length - 1;
+        int end = right - left - 1;
 	
         while (dataIndex < right) {
 	    
@@ -54,8 +53,8 @@ public class Quick {
             }
 	    printArray(tempArray);
 	    dataIndex++;
-	    
 	}
+
 
 	for (int i = start; i <= end; i++) {
 	    tempArray[i] = pivot;
@@ -68,7 +67,6 @@ public class Quick {
 	    tempIndex++;
 	}
 
-	printArray(tempArray);
 	printArray(data);
 
 	return new int[]{start, end};
@@ -92,12 +90,35 @@ public class Quick {
         data[b] = temp;
     }
 
-    /*
+    
     public static int quickSelect(int[] data, int k) {
 
         return quickSelect(data, k - 1, 0, data.length - 1);
     }
+    
+    private static int quickSelect(int[] data, int k, int left, int right) {
+	int[] indices =  partition(data, left, right);
+	int startIndex = indices[0];
+	int endIndex = indices[1];
 
+        if (k >= startIndex && k <= endIndex) {
+            return data[startIndex];
+        } else if (startIndex> k) {
+            return quickSelect(data, k, left, startIndex - 1);
+        } else if (endIndex < k) {
+            return quickSelect(data, k, endIndex + 1, right);
+        }
+	
+        return 0;
+
+    }
+    
+    /*
+    public static int quickSelectOld(int[] data, int k) {
+
+        return quickSelectOld(data, k - 1, 0, data.length - 1);
+    }
+    
     
     private static int quickSelectOld(int[] data, int k, int left, int right) {
         int index =  partitionOld(data, left, right);
@@ -105,47 +126,28 @@ public class Quick {
         if (index == k) {
             return data[index];
         } else if (index > k) {
-            return quickSelect(data, k, left, index - 1);
+            return quickSelectOld(data, k, left, index - 1);
         } else if (index < k) {
-            return quickSelect(data, k, index + 1, right);
-        }
-	
-        return 0;
-
-    }
-
-    
-    private static int quickSelect(int[] data, int k, int left, int right) {
-	int[] indices =  partition(data, left, right);
-	
-        if (index == k) {
-            return data[index];
-        } else if (index > k) {
-            return quickSelect(data, k, left, index - 1);
-        } else if (index < k) {
-            return quickSelect(data, k, index + 1, right);
+            return quickSelectOld(data, k, index + 1, right);
         }
 	
         return 0;
 
     }
     */
-
     public static void quickSort(int[] data) {
         quickSort(data, 0, data.length - 1);
     }
 
     private static void quickSort(int[] data, int left, int right) {
-        if (left < right) {
-            int[] indices = partition(data, left, right);
-            int startIndex = indices[0];
-	    int endIndex = indices[1];
-	    System.out.println(startIndex);
-	    System.out.println(endIndex);
+	int[] indices = partition(data, left, right);
+	int startIndex = indices[0];
+	int endIndex = indices[1];
+	if (left < right) {
 	    if (startIndex - 1 > left) {
-		//		quickSort(data, left, startIndex - 1);
-            } else if (endIndex + 1 < right) {
-		//quickSort(data, endIndex + 1, right);
+		quickSort(data, left, startIndex - 1);
+            } else if (endIndex  < right) {
+		quickSort(data, 1 + endIndex, right);
 	    }
 	} 
     }
@@ -190,8 +192,11 @@ public class Quick {
     
     public static void main(String[] args) {
         int[] d1 = {1, 7, 2, 2, 4, 6, 2 , 3, 2, 13, 19, 86};
-	quickSort(d1);
-	//	System.out.println(isSorted(d1));
+	int[] data = new int[4000000];
+	fillRandom(data);
+	//partition(d1, 0, d1.length);
+	quickSort(data);
+	System.out.println(isSorted(d1));
 	
 	//printArray(d1);
         //quickSort(d1);
