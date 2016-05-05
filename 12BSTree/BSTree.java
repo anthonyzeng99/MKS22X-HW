2bd567437de88b50af976e2f533bbdc6fc837138
Anthony Zeng <anthonyzeng99@gmail.com>
2016-05-05 09:16:31 -0400

    working BSTree add method

--- 12BSTree/BSTree.java
+++ 12BSTree/BSTree.java
@@ -26,80 +26,78 @@ public class BSTree<T extends Comparable<T>> {
 	}
 
 	public void add(Node current, T value) {
-	    boolean added = false;
-	    while ( !added ) {
-		if ( value.compareTo(current.data) < 0 ) {
-		    if (current.left == null ) {
-			current.left = new Node(value);
-			added = true;
-			System.out.println("added left");
-		    } else { 
-			System.out.println("<");
-			add(current.left,value);
-		    }		    
-		} else if ( value.compareTo(current.data) > 0 ) {
-		    if (current.right == null) {
-			current.right = new Node(value);
-			added = true;
-			System.out.println("added right");
-		    } else {
-			System.out.println(">");
-			add(current.right,value);
-		    }
-		}	
-	    }
+	    
+	    if ( value.compareTo(current.data) < 0 ) {
+		if (current.left == null ) {
+		    current.left = new Node(value);
+		    System.out.println("added left");
+		} else { 
+		    System.out.println("<");
+		    add(current.left,value);
+		}		    
+	    } else if ( value.compareTo(current.data) > 0 ) {
+		if (current.right == null) {
+		    current.right = new Node(value);
+		    System.out.println("added right");
+		} else {
+		    System.out.println(">");
+		    add(current.right,value);
+		}
+	    }	
 	}
 
+	
+	
 	public String toString() {
 	    return "";
 	}
-
+	
 	public boolean contains(T value) {
 	    return false;
 	}
-
+	
     }
-
-    private Node root;
-
-    public BSTree(T data) {
-	root = new Node(data);
+    
+	private Node root;
+	
+	public BSTree(T data) {
+	    root = new Node(data);
+	}
+	
+	public void add(T value) {
+	    root.add(value);
+	}
+	
+	public String toString() {
+	    return root.toString();
+	}
+	
+	public boolean contains(T value) {
+	    return root.contains(value);
+	}
+	
+	public int getHeight() {
+	    return root.height();
+	}
+	
+	public static void main(String[] args) {
+	    BSTree<Integer> tree = new BSTree<Integer>(5);
+	    tree.add(6);
+	    tree.add(2);
+	    tree.add(3);
+	    tree.add(12);
+	    tree.add(72);
+	    tree.add(91);
+	    tree.add(32);
+	    tree.add(8);
+	    tree.add(0);
+	    tree.add(38);
+	    tree.add(23);
+	    tree.add(26);
+	    tree.add(13);
+	    tree.add(18);
+	    tree.add(62);
+	    tree.add(85);
+	}
+	
     }
-
-    public void add(T value) {
-	root.add(value);
-    }
-
-    public String toString() {
-	return root.toString();
-    }
-
-    public boolean contains(T value) {
-	return root.contains(value);
-    }
-
-    public int getHeight() {
-	return root.height();
-    }
-
-    public static void main(String[] args) {
-	BSTree<Integer> tree = new BSTree<Integer>(5);
-	tree.add(6);
-	tree.add(2);
-	tree.add(3);
-	tree.add(12);
-	tree.add(72);
-	tree.add(91);
-	tree.add(32);
-	tree.add(8);
-	tree.add(0);
-	tree.add(38);
-	tree.add(23);
-	tree.add(26);
-	tree.add(13);
-	tree.add(18);
-	tree.add(62);
-	tree.add(85);
-    }
-
-}
