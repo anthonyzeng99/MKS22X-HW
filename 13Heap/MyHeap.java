f9a18583de95bb0e7bf98fefee9f47497f99bfcd
Anthony Zeng <anthonyzeng99@gmail.com>
2016-05-09 00:33:25 -0400

    worked on Heap

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -3,52 +3,140 @@ import java.util.*;
 public class MyHeap<T extends Comparable<T>> {
    private int size;
    private T[] data;
+   private boolean isMax;
 
    public MyHeap() {
        data = (T[]) new Comparable[100];
-       data[0] == null;
+       isMax = true;
    }
 
    public MyHeap(T[] array) {
-       data = new T[array.length + 1];
+       data = (T[]) new Comparable[array.length + 1];
        for (int i = 1; i < data.length; i++) {
 	   data[i] = array[i - 1];
        }
+       size = array.length;
+       isMax = true;
        heapify();
    }
 
+      //do this last
+   public MyHeap(boolean isMax) {
+       data = (T[]) new Comparable[100];
+       this.isMax = isMax;
+   }
+   
+   public MyHeap(T[] array, boolean isMax) {
+       data = (T[]) new Comparable[array.length + 1];
+       for (int i = 1; i < data.length; i++) {
+	   data[i] = array[i - 1];
+       }
+       size = array.length;
+       this.isMax = isMax;
+       heapify();
+   }
+
+
+
    private void pushDown(int index) {
-       int leftValue = data[index * 2];
-       int rightValue = data[(index * 2) + 1];
-       if (data[index] < leftValue && data[index] < rightValue) {
+       T leftValue = null;
+       T rightValue = null;
+       T temp = data[index];
+       //Checks if parent has left or right child assigns the variable their value
+       if (size + 1 > index * 2) {
+	   leftValue = data[index * 2];
+       }
+       if (size + 1 > (index * 2) + 1) {
+	   rightValue = data[(index * 2) + 1];
+       }
+
+       if(leftValue == null || rightValue == null) {
+	   if (leftValue == null) {
+	       if (rightValue != null && data[index].compareTo(rightValue) > 0) {
+		   data[index] = data[(index * 2) + 1];
+		   data[(index * 2) + 1] = temp;
+		   pushDown( (index * 2) + 1);
+	       } 
+	   }else if (rightValue == null) {
+	       if (leftValue != null && data[index].compareTo(leftValue) > 0) {
+		   data[index] = data[index * 2];
+		   data[index * 2] = temp;
+		   pushDown( index * 2);
+	       } 
+	   }
 	   return;
-       } else if (data[index] < leftValue
+       }
+       
+       int difference = leftValue.compareTo(rightValue);
+       
+       if (difference < 0) {
+	   
+	   if (leftValue != null && data[index].compareTo(leftValue) > 0) {
+	       data[index] = data[index * 2];
+	       data[index * 2] = temp;
+	       pushDown(index * 2);
+	   } else if (rightValue != null && data[index].compareTo(rightValue) > 0) {
+	       data[index] = data[(index * 2) + 1];
+	   data[(index * 2) + 1] = temp;
+	   pushDown( (index * 2) + 1);
+	   }	   
+       }
+
+       if (rightValue != null && data[index].compareTo(rightValue) > 0) {
+	   data[index] = data[(index * 2) + 1];
+	   data[(index * 2) + 1] = temp;
+	   pushDown( (index * 2) + 1);
+
+       } else if (leftValue != null && data[index].compareTo(leftValue) > 0) {
+	   data[index] = data[index * 2];
+	   data[index * 2] = temp;
+	   pushDown(index * 2);
+       }
+       
+       return;
    }
 
    
 
    private void pushUp(int index) {
        int parent = index / 2;
-       if (data[index] > data[parent]) {
+       if (data[index].compareTo(data[parent]) > 0) {
 	   return;
        } 
-       int temp = data[index];
+       T temp = data[index];
        data[index] = data[parent];
        data[parent] = temp;
        pushUp(parent);
    }
 
    private void heapify() {
-       
+       for (int i = 1; i < size + 1/ 2; i++) {
+	   pushDown(i);
+       }
    }
 
    public T delete() {
-       T a;
-       return a;
+       if (data.length == 0) {
+	   throw new NoSuchElementException();
+       }
+       T deletedValue = data[1];
+       T[] newData = (T[]) new Comparable[data.length - 1];
+       for (int i = 2; i < data.length; i++) {
+	   newData[i - 1] = data[i];
+       }
+       data = newData;
+       size--;
+       return deletedValue;
+       
    }
 
    public void add(T x) {
-       for  
+       if (size + 1 >= data.length - 1) {
+	   doubleSize();
+       }
+       data[size + 1] = x;
+       size++;
+       heapify();
    }
 
    private void doubleSize() {
@@ -57,17 +145,20 @@ public class MyHeap<T extends Comparable<T>> {
    }
 
    public String toString() {
-       return "";
+       String output = "[";
+       for (int i = 1; i < size + 1; i++) {
+	   output += data[i] + ", ";
+       }
+       output = output.substring(0, output.length() - 2);
+       return output + "]";
    }
 
-   //do this last
-   public MyHeap(boolean isMax) {
+   public static void main(String[] args) {
+       Integer[] i = {5, 30, 17, 6, 8, 23, 1};
+       MyHeap m = new MyHeap(i);
+       m.add(2);
+       System.out.println(m);
 
    }
-
-   public MyHeap(T[] array, boolean isMax) {
-
-   }
-
 
 }
