54a48215a3bda57abf2fe6f65bb93ab4a03e92c9
Anthony Zeng <anthonyzeng99@gmail.com>
2016-05-15 22:43:02 -0400

    RunningMedian

--- 14Median/RunningMedian.java
+++ 14Median/RunningMedian.java
@@ -1,7 +1,12 @@
+import java.util.*;
+
 public class RunningMedian {
 
     private MyHeap<Integer> smaller;
     private MyHeap<Integer> larger;
+    private int smallSize;
+    private int largeSize;
+    private int median;
 
     public RunningMedian() {
 	smaller = new MyHeap<Integer>(true);
@@ -9,16 +14,46 @@ public class RunningMedian {
     }
 
     public double getMedian() {
-	if (smaller.getSize() > larger.getSize()) {
-	    return smaller.peek();
-	} else if (larger.getSize() > smaller.getSize()) {
-	    return larger.peek();
+	if (smallSize == 0 && largeSize == 0) {
+	    throw new NoSuchElementException();
 	}
-	return (smaller.peek() + larger.peek()) / 2;
+	return median;
     }
 
     public void add(Integer x) {
-	
+	if (smallSize == 0 && largeSize == 0) {
+	    smaller.add(x);
+	    smallSize++;
+	    median = x;
+	    
+	} else {
+	    if (x < median) {
+		smaller.add(x);
+		smallSize++;
+	    } else {
+		larger.add(x);
+		largeSize++;
+	    }
+	}
+
+	if (Math.abs(largeSize - smallSize) > 1) {
+	    if (largeSize > smallSize) {
+		for (int i = (largeSize - smallSize) / 2; i > 0; i++) {
+		    int temp = larger.delete();
+		    smaller.add(temp);
+		    largeSize--;
+		    smallSize++;
+		    
+		}
+	    } else {
+		for (int i = (smallSize - largeSize) / 2; i > 0; i++) {
+		    int temp = smaller.delete();
+		    larger.add(temp);
+		    smallSize--;
+		    largeSize++;
+		}
+	    }
+	}
     }
 
     public static void main(String[] args) {
